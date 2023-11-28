1b553b61b3d1a9ed1cda54d4c9cfd8f6b2839c9f
pablekke <sb.paablo@gmail.com>
2023-11-28 20:03:49 -0300

    28/11 usuarios y sistema

--- Dominio/Usuario.cs
+++ Dominio/Usuario.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,13 +10,80 @@ namespace Dominio
     internal class Usuario
     {
         public int UltimoId = 1;
-        public int Id { get; set; }
-        public string? Nombre { get; set; }
-        public string? Apellido { get; set; }
-        public string? Email  { get; set; }
-        public string? Foto { get; set; }
-        public string? Pass { get; set; }
+        internal int Id { get; set; }
+        internal string? Foto { get; set; }
+        internal string? Nombre { get; set; }
+        internal string? Apellido { get; set; }
+        internal string? Email { get; set; }
+        internal string? Pass { get; set; }
+        internal bool Bloqueado { get; set; }
 
+        internal Usuario(string? foto, string? nombre, string? apellido, string? email, string? pass)
+        {
+            Id = UltimoId++;
+            Foto = foto;
+            Nombre = nombre;
+            Apellido = apellido;
+            Email = email;
+            Pass = pass;
+            Bloqueado = false;
+        }
 
-}
+        internal Usuario()
+        {
+            Id = UltimoId++;
+            Bloqueado = false;
+        }
+
+        internal bool ValidarCredenciales(string e, string p)
+        {
+            if (string.IsNullOrEmpty(e))
+            {
+                throw new ArgumentNullException("Email vacío.");
+            }
+            if (string.IsNullOrEmpty(p))
+            {
+                throw new ArgumentNullException("Contraseña vacía.");
+            }
+            return (e == Email && p == Pass);
+        }
+
+        internal void Bloquear()
+        {
+            Bloqueado = true;
+        }
+        internal void Desbloquear()
+        {
+            Bloqueado = false;
+        }
+
+        internal virtual void EsValido()
+        {
+            if (string.IsNullOrEmpty(Nombre))
+            {
+                throw new ArgumentNullException("Nombre vacío.");
+            }
+            if (string.IsNullOrEmpty(Apellido))
+            {
+                throw new ArgumentNullException("Apellido vacío.");
+            }
+            if (string.IsNullOrEmpty(Email))
+            {
+                throw new ArgumentNullException("Email vacío.");
+            }
+            if (string.IsNullOrEmpty(Pass))
+            {
+                throw new ArgumentNullException("Contraseña vacía.");
+            }
+            if (string.IsNullOrEmpty(Foto))
+            {
+                throw new ArgumentNullException("Nombre vacío.");
+            }
+
+        }
+        public override string ToString()
+        {
+            return $"Id: {Id}, Foto: {Foto ?? "N/A"}, Nombre: {Nombre ?? "N/A"}, Apellido: {Apellido ?? "N/A"}, Email: {Email ?? "N/A"}, Bloqueado: {Bloqueado}";
+        }
+    }
 }
