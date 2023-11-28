6c97d6f7fa95c22b47790b70a044b7208c96b4ad
Matias <csmatiperreng@gmail.com>
2023-11-28 18:51:36 -0300

    conexiónPreparada

---
+++ WebAppReact/Controllers/TareaController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApp;
+
+namespace WebAppReact.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class TareaController : ControllerBase
+	{
+
+
+	}
+}
---
+++ WebAppReact/Models/Usuario.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApp;
+
+public partial class Usuario
+{
+    public int Id { get; set; }
+}
