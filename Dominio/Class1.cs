5586591b6a1f686bc3bc27599b21cfc21e19c2d8
pablekke <sb.paablo@gmail.com>
2023-11-27 22:46:49 -0300

    27/11/2023

--- Dominio/Class1.cs
+++ Dominio/Class1.cs
@@ -1,7 +1,20 @@
 ﻿namespace Dominio
 {
-    public class Class1
+    public class Sistema
     {
+        #region Singleton
+        private Sistema() {
+            //Precarga();
+        }
+        private static Sistema instance = null;
+        public static Sistema GetSistema() { 
+            if (instance == null)
+            {
+                instance = new Sistema();
+            }
+            return instance;
+        }
+        #endregion
 
     }
 }
