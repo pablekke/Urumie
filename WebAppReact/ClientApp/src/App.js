35acaf002b107902362855990ea9137ab470a571
pablekke <sb.paablo@gmail.com>
2023-11-28 21:28:58 -0300

    kkkk

--- WebAppReact/ClientApp/src/App.js
+++ WebAppReact/ClientApp/src/App.js
@@ -7,7 +7,11 @@ const App = () => {
         <div>
             <h1 className='text-danger'>Hola mundo</h1>
             <h2>Te imaginas ser el luk</h2>
+            <div>
+                <p>Pobre luk, mucho auto poca pepa</p>
+            </div>
         </div>
+
     )
 }
 export default App;
